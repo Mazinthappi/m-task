5d5b32cf665d9af6507816e6486d8279dc6a01b2
Mazinthappi <mazinthappi@gmail.com>
2025-09-21 20:01:00 +0530

    change seeder name

--- database/seeders/AdminProfileSeeder.php
+++ database/seeders/AdminProfileSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class UserProfileSeeder extends Seeder
+class AdminProfileSeeder extends Seeder
 {
     /**
      * Run the database seeds.
